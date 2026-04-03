68bf61d21b671d221e03f264642d060e5b57990a
root <gitlab_admin_ec5af3@example.com>
2026-04-03 20:08:55 +0800

    cdv1.0

---
+++ src/main/java/com/example/Myapp.java
@@ -0,0 +1,21 @@
+package com.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Myapp {
+    public static void main(String[] args) {
+        SpringApplication.run(Myapp.class, args);
+    }
+    @GetMapping("/")
+    public String home() {
+    return "Welcome to CI/CD Demo!";
+}
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello CI/CD & Docker & Nginx!";
+    }
+}
